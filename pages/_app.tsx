7f839018851172e8753a58ba19bce91ae031bc77
xiongjiajun <xiongjiajun0912@qq.com>
2022-11-22 22:05:16 +0800

    [dev]import antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { ConfigProvider, theme } from 'antd'
 
+const { darkAlgorithm, compactAlgorithm }= theme;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider theme={{
+      algorithm: [darkAlgorithm, compactAlgorithm],
+      token: {
+        colorPrimary: "#f00"
+      }
+    }}>
+      <Component {...pageProps} />
+    </ConfigProvider> 
+  )
 }
